e0bb0db0063b76faf9558ff58c7494bbede97e97
Shreyansh <shreyansh.kumar32@outlook.com>
2023-03-11 17:41:06 +0530

    Addes JS topics

---
+++ 2variable.js
@@ -0,0 +1,23 @@
+// https://www.w3schools.com/js/js_variables.asp
+// This link helps us to understand varibles in JS
+
+// There are many types of varibles which we use in JS
+// let , var and const
+
+
+let num =4+7;
+console.log(num);
+
+let userName = "Shreyansh's work Phone";
+console.log(userName)
+
+
+// Constant
+
+// find a area of circle 
+let radius = 4;
+const PI = 3.14;
+let area = PI *radius*radius;
+
+console.log(area);
+
---
+++ 3dataType.js
@@ -0,0 +1,23 @@
+
+
+/* There are two types of data in JavaScirpt
+
+1. Primitive
+    a. Number
+    b. String
+    c. Boolean
+    d. Null
+    e. Undedfiend
+    f. Symobol
+
+
+2. Object
+*/
+
+// if we want to print long number as it is then we need to add n in last like below 
+let num = 9432649783265827356823756n;
+console.log(num+8n);
+
+// With help of typeof we can check datatype of any varibale
+console.log(typeof num);
+
