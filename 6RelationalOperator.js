0b756a13ed72b1effabe44267204675073dae7b5
Shreyansh <shreyansh.kumar32@outlook.com>
2023-03-12 19:14:32 +0530

    added some code

---
+++ 6RelationalOperator.js
@@ -0,0 +1,49 @@
+
+
+
+
+// Number number comparison
+let data = 6>=6;
+console.log(data);
+
+
+/*
+
+When we are comparing two strings values in JavaScript it will compare 
+ASCII values charecter by charecter
+
+In below example A's ASCII value is less than K that's why it will
+give us result as a false
+
+*/ 
+let str1 = "Atring";
+let str2 = "Kumar";
+console.log(str1>str2);
+
+
+/**
+   If we compare string and number in javaScript then it will compare
+   String into Number and then Compare like below example.
+ */
+let x = 67
+let y = "65";
+console.log(x>y);
+
+
+
+/**
+ *     ==   -> when we are using this it will comparing the data not type.
+ *     ===  -> when we are using this it will comparing the data as well as type.
+ *   
+ *  
+ *     Like below example
+ *  
+ */
+
+
+let s ="56";
+let s1 =56;
+console.log(s==s1);
+console.log(s===s1);
+
+
