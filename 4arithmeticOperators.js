03fac83830177381b71fff2b36847fa633d34c1d
Shreyansh <shreyansh.kumar32@outlook.com>
2023-03-12 16:43:05 +0530

    aithmetic added

--- 4arithmeticOperators.js
+++ 4arithmeticOperators.js
@@ -1,4 +1,34 @@
 // Link -> https://www.w3schools.com/js/js_arithmetic.asp
 
+/*
 
++	Addition
+-	Subtraction
+*	Multiplication
+**	Exponentiation (ES2016)
+/	Division
+%	Modulus (Remainder)
+++	Increment
+--	Decrement
+
+*/
+
+let num1 = 78;
+let num2 = 89;
+
+console.log(num1+num2);
+
+let num3 = true;
+let num4 = true;
+console.log(num3+num4);
+
+
+let n = 4;
+
+let x = n++;
+console.log(x,n);
+
+let n1 =4
+let x1 = ++n1;
+console.log(x1,n);
 
