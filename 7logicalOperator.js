c7d3af315ab000b85fd5eedc7c972d1ac0e174ea
Shreyansh <shreyansh.kumar32@outlook.com>
2023-03-13 11:02:22 +0530

    added condutional and logical operators

---
+++ 7logicalOperator.js
@@ -0,0 +1,29 @@
+/*
+
+Operator	Description	Comparing	Returns	Try it
+==	        equal to	x == 8	        false	
+                        x == 5	true	
+                        x == "5"	true
+
+===	        equal value and equal type	    x === 5 	true	
+                                            x === "5"	false
+
+!=	        not equal	                         x != 8	                        true	
+!==	        not equal value or not equal type	x !== 5	    false	
+                                                x !== "5"	true	
+                                                x !== 8	true	
+>	greater than	x > 8	false	
+<	less than	x < 8	true	
+>=	greater than or equal to	x >= 8	false	
+<=	less than or equal to	x <= 8	true
+
+*/
+
+let x=87, y=8,z=90
+
+let result = (x>y) <z;
+console.log(result);
+
+
+let result1 = x<y || x<z;
+console.log(result1);
---
+++ 8conditionalStatement.js
@@ -0,0 +1,19 @@
+/*
+
+Link -> https://www.tutorialspoint.com/What-is-Conditional-Operator-in-JavaScript
+
+
+
+*/
+
+let num1 = 69
+let num2 = 69
+
+if(num1>num2)
+    console.log("True");
+
+else if(num2>num1)
+    console.log("Nothing");
+
+else
+    console.log("False");    
